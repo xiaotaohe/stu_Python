5295cad770054e9cab032974491440c661fa7eda
xiaotaohe <taochao1997@qq.com>
2019-05-09 14:51:28 +0800

    string，变量，注释等的学习

---
+++ python-project/stu_string/stu_string.py
@@ -0,0 +1,34 @@
+# coding=utf8
+'''
+Created on 2019��5��9��
+
+@author: Administrator
+'''
+print("hello world!");
+print('hello world!');
+'''
+多行打印
+'''
+
+print('''hello world,
+        hello python,
+        hello string!''');
+age = 3
+name = "Tom"
+
+'''
+format语法
+'''
+
+print("{0} was {1} years old".format(name,age))
+print(name+" was "+str(age)+" years old!")
+
+
+'''
+非法命名
+'''
+
+'''
+2people = 3
+'''
+# 你好
